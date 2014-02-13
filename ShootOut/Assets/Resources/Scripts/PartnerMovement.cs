e74b0f4364cfc4a45c69368a098376afffd4025c
Rxanadu <ed.onuk31@gmail.com>
2014-02-13 18:56:55 -0500

    added ability to fire from mouse position, new partner movment system implemented

---
+++ ShootOut/Assets/Resources/Scripts/PartnerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartnerMovement : MonoBehaviour
+{
+
+    public int movementSmoothing;
+    public float speed = 60f;
+    Vector3 targetPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            movementSmoothing = 1;
+            Plane playerPlane = new Plane(Vector3.up, transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitDist = 0.0f;
+
+            if (playerPlane.Raycast(ray, out hitDist))
+            {
+                var targetPoint = ray.GetPoint(hitDist);
+                targetPosition = targetPoint;
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                transform.rotation = targetRotation;
+            }
+        }
+
+        Vector3 dir = targetPosition - transform.position;
+        float dist = dir.magnitude;
+        
+        float move = speed * Time.deltaTime;
+        if (dist > move)
+        {
+            transform.position += dir.normalized * move;
+        }
+        else
+        {
+            transform.position = targetPosition;
+        }
+    }
+}
---
+++ ShootOut/Assets/Resources/Scripts/ProjectileMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMovement : MonoBehaviour
+{
+
+    public float force = 10f;
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(transform.forward * force, ForceMode.Acceleration);
+    }
+
+    void OnTriggerEnter()
+    {
+        Destroy(gameObject);
+    }
+}
