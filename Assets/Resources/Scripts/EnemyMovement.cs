bf47bf466a8bac8fb5449d03eb82283d2f3bcaa2
Rxanadu <ed.onuk31@gmail.com>
2014-02-15 18:49:39 -0500

    added enemy movement

---
+++ Assets/Resources/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    Transform enemyTransform;
+
+    public float speed = 3f;
+    public float hitDistance = 3f;
+
+    void Start()
+    {
+        enemyTransform = transform.parent;
+    }
+
+    void Update()
+    {
+        Vector3 fwd = Vector3.forward;
+        Debug.DrawLine(transform.position, fwd, Color.red);
+
+        float step = speed * Time.deltaTime;
+
+        if (Physics.Raycast(transform.position, fwd, hitDistance))
+        {
+            /*if(enemyTransform.transform.rotation.y>0){
+                enemyTransform.transform.Rotate (Vector3.up*40*Time.deltaTime);
+            }
+            if(enemyTransform.transform.rotation.y>0){
+                enemyTransform.transform.Rotate (Vector3.up*-40*Time.deltaTime);
+            }*/
+            enemyTransform.transform.Rotate(0, -enemyTransform.transform.rotation.y * Time.deltaTime, 0);
+        }
+        else
+        {
+            enemyTransform.Translate(Vector3.forward * step);
+        }
+    }
+}
--- Assets/Resources/Scripts/PartnerMovement.cs
+++ Assets/Resources/Scripts/PartnerMovement.cs
@@ -3,40 +3,68 @@ using System.Collections;
 
 public class PartnerMovement : MonoBehaviour
 {
-
-    public int movementSmoothing;
     public float speed = 60f;
+    public GameObject movementMarker;
     Vector3 targetPosition;
+    Quaternion targetRotation;
+    Quaternion markerRotation;
+    GameObject markerClone;
+
+    void Start() {
+        targetPosition = transform.position;
+        markerRotation = Quaternion.Euler(90, 0, 0);
+
+        if (movementMarker != null)
+        {
+            markerClone = Instantiate(movementMarker, targetPosition, Quaternion.identity) as GameObject;
+            markerClone.transform.rotation = markerRotation;
+            markerClone.renderer.enabled = false;
+        }            
+    }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            movementSmoothing = 1;
-            Plane playerPlane = new Plane(Vector3.up, transform.position);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float hitDist = 0.0f;
-
-            if (playerPlane.Raycast(ray, out hitDist))
-            {
-                var targetPoint = ray.GetPoint(hitDist);
-                targetPosition = targetPoint;
-                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-                transform.rotation = targetRotation;
-            }
+            SetTargetLocation();
         }
 
-        Vector3 dir = targetPosition - transform.position;
-        float dist = dir.magnitude;
-        
-        float move = speed * Time.deltaTime;
-        if (dist > move)
+        MovePartnerToTargetLocation();
+    }
+
+    void SetTargetLocation() {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitDist))
         {
-            transform.position += dir.normalized * move;
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Vector3 newTargetPoint = new Vector3(targetPoint.x, transform.position.y, targetPoint.z);
+            targetPosition = newTargetPoint;
+
+            Quaternion targetRotation = Quaternion.LookRotation(newTargetPoint - transform.position);
+            transform.rotation = targetRotation;
+
+            markerClone.transform.position = targetPosition;
+            markerClone.transform.rotation = markerRotation;
+            markerClone.renderer.enabled = true;
+        }
+    }
+
+    void MovePartnerToTargetLocation() {
+        Vector3 direction = targetPosition - transform.position;
+        float distance = direction.magnitude;
+        float step = speed * Time.deltaTime;
+
+        if (distance > step)
+        {
+            transform.position += direction.normalized * step;
         }
         else
         {
             transform.position = targetPosition;
+            markerClone.renderer.enabled = false;
         }
     }
 }
